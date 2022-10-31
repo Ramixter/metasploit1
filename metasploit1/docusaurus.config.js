2d0873ec4623738f16f66c86dfadc6a0d83502c8
Ramiro Torres <ramixalex@hotmail.com>
2022-10-31 17:29:40 +0100

    Update docusaurus.config.js

--- metasploit1/docusaurus.config.js
+++ metasploit1/docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Ramiro Torres',
+  tagline: 'Learning about Cybersecurity',
   url: 'https://Ramixter.github.io',
   baseUrl: '/metasploit1/',
   onBrokenLinks: 'throw',
